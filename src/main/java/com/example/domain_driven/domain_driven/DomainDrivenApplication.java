f919f0a5e6151fe2470a7ae90d0a772c1024f18f
iseungmin <sm1111176@gmail.com>
2025-09-15 23:08:38 +0900

    init

---
+++ src/main/java/com/example/domain_driven/domain_driven/DomainDrivenApplication.java
@@ -0,0 +1,13 @@
+package com.example.domain_driven.domain_driven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainDrivenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainDrivenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/domain_driven/domain_driven/DomainDrivenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domain_driven.domain_driven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainDrivenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
